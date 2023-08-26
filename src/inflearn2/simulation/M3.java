b1846841aeae29c50fa35c791ba3e0497a5fccd2
juno-choi <ililil9482@naver.com>
2023-08-26 15:21:18 +0900

    [잃어버린 강아지] 잃어버린 강아지 문제 풀이
    https://www.inflearn.com/course/lecture?courseSlug=%EC%9E%90%EB%B0%94-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%B5%9C%EC%8B%A0%EA%B8%B0%EC%B6%9C&unitId=149828&tab=curriculum

--- src/inflearn2/simulation/M3.java
+++ src/inflearn2/simulation/M3.java
@@ -2,113 +2,81 @@ package inflearn2.simulation;
 
 public class M3 {
     public static void main(String[] args) {
-        int[][] board = {
-                {1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-                {0, 0, 1, 1, 0, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 1, 0, 1, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 0, 0, 0, 0, 2, 1},
-                {0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
-                {0, 1, 0, 1, 0, 0, 0, 0, 0, 3}
-        };
+        int[][] board = {{0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 2, 0, 0}, {1, 0, 0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 3, 0, 0, 0, 1}, {0, 0, 0, 1, 0, 1, 0, 0, 0, 0}, {0, 1, 0, 1, 0, 0, 0, 0, 0, 0} };
 
         System.out.println(solution(board));
     }
-    public static int solution(int[][] board){
+
+    public static int solution(int[][] board) {
         int answer = 0;
 
-        int[][] personDirect = {{-1,0},{0,1},{1,0},{0,-1}};
-        int[] person = {0,0};
+        int[][] personDirect = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int[] person = {0, 0};
         int personDirectIndex = 0;
 
-        int[][] dogDirect = {{-1,0},{0,1},{1,0},{0,-1}};
-        int[] dog = {0,0};
+        int[][] dogDirect = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int[] dog = {0, 0};
         int dogDirectIndex = 0;
 
 
         // 현수 2 찾기
         int boardLength = board.length;
-        for(int i=0; i<boardLength; i++){
+        for (int i = 0; i < boardLength; i++) {
             int length = board[i].length;
-            for(int j=0; j<length; j++){
+            for (int j = 0; j < length; j++) {
                 int su = board[i][j];
-                if(su == 2){
+                if (su == 2) {
                     person[0] = i;
                     person[1] = j;
-                }else if(su == 3){
+                } else if (su == 3) {
                     dog[0] = i;
                     dog[1] = j;
                 }
-
             }
         }
-        // 강아지 3 찾기
-        for(int i=0; i<10000; i++){
-            int personX = person[0]+personDirect[personDirectIndex][0];
-            int personY = person[1]+personDirect[personDirectIndex][1];
+
+        for (int i = 0; i < 10000; i++) {
+            answer++;
+
+            int personX = person[0] + personDirect[personDirectIndex][0];
+            int personY = person[1] + personDirect[personDirectIndex][1];
             boolean personStay = false;
 
-            int dogX = dog[0]+dogDirect[dogDirectIndex][0];
-            int dogY = dog[1]+dogDirect[dogDirectIndex][1];
+            int dogX = dog[0] + dogDirect[dogDirectIndex][0];
+            int dogY = dog[1] + dogDirect[dogDirectIndex][1];
             boolean dogStay = false;
 
-            if(personX>= boardLength || personY>= boardLength || personX<0 || personY<0){
+            if (personX >= boardLength || personY >= boardLength
+                    || personX < 0 || personY < 0
+                    || board[personX][personY] == 1
+            ) {
                 personDirectIndex++;
+                personDirectIndex = personDirectIndex >= personDirect.length ? 0 : personDirectIndex;
                 personStay = true;
-                if(personDirectIndex == 4){
-                    personDirectIndex= 0;
-                }
             }
-            if(dogX>= boardLength || dogY>= boardLength || dogX<0 || dogY<0){
+
+            if (dogX >= boardLength || dogY >= boardLength
+                    || dogX < 0 || dogY < 0
+                    || board[dogX][dogY] == 1
+            ) {
                 dogDirectIndex++;
+                dogDirectIndex = dogDirectIndex >= dogDirect.length ? 0 : dogDirectIndex;
                 dogStay = true;
-                if(dogDirectIndex == 4){
-                    dogDirectIndex= 0;
-                }
-            }
-            if(personStay){
-                personX = person[0];
-                personY = person[1];
-            }
-            if(dogStay){
-                dogX = dog[0];
-                dogY = dog[1];
             }
 
-            int personSu = board[personX][personY];
-            if(personSu == 1 && !personStay){
-                personDirectIndex++;
-                personStay = true;
-                if(personDirectIndex == 4){
-                    personDirectIndex= 0;
-                }
+            if (!personStay) {
+                person[0] = personX;
+                person[1] = personY;
             }
 
-            int dogSu = board[dogX][dogY];
-            if(dogSu == 1 && !dogStay){
-                dogDirectIndex++;
-                dogStay = true;
-                if(dogDirectIndex == 4){
-                    dogDirectIndex= 0;
-                }
+            if (!dogStay) {
+                dog[0] = dogX;
+                dog[1] = dogY;
             }
-
-            person[0] = personX;
-            person[1] = personY;
-
-            dog[0] = dogX;
-            dog[1] = dogY;
-
-
 
             if(person[0] == dog[0] && person[1] == dog[1]){
                 break;
             }
-
-            answer++;
         }
         return answer == 9999 ? 0 : answer;
     }
